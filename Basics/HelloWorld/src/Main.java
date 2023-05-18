4b66db443969453775f6d5c09ad399403e33ab99
ronak <08arnab98@gmail.com>
2023-05-19 03:13:49 +0530

    Hello World in Java

--- Basics/HelloWorld/src/Main.java
+++ Basics/HelloWorld/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        //The basic boilerplate and Hello world code in Java
         System.out.println("Hello world!");
     }
 }
