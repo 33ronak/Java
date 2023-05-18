d2f6acbd7b891425f4e4a178e3238f8a1bdd03f1
Arnab Choudhury <08arnab98@gmail.com>
2023-05-19 04:10:33 +0530

    Delete Basics directory

--- Basics/HelloWorld/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //The basic boilerplate and Hello world code in Java
-        System.out.println("Hello world!");
-    }
-}
