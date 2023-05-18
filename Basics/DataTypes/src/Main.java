07fc6f67f1519e7420c45751e0f9a3476b4ad210
ronak <08arnab98@gmail.com>
2023-05-19 03:09:42 +0530

    Basic data types in Java

---
+++ Basics/DataTypes/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Integer data type is generally used for numeric value
+        // The most common ones are:
+        // byte     1 byte
+        // short    2 byte
+        // int      4 byte
+        // long     8 byte
+        byte b = 50;
+        short s = 32750;
+        int i = 83500;
+        long l = 959497505 ;
+
+
+        // Floating point number //
+        // float    4 bytes: for float use 'f' as suffix as standard
+        // double   8 bytes: by default fraction value by default fraction value
+        float f = 4.7333434f;
+        double d = 4.355453532;
+
+
+        // Creating and initializing custom character
+        char a = 'A';
+
+        System.out.println("char: " + a);
+        System.out.println("integer: " + i);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("long: " + l);
+    }
+}
